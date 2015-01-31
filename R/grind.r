22a1a1f542381271c09ba3fd58d97e02bc793ae8
Mike <MikeXL@users.noreply.github.com>
2015-01-30 22:39:12 -0800

    Create grind.r

---
+++ R/grind.r
@@ -0,0 +1,12 @@
+#' A Grind Function
+#'
+#' This function allows you to grind your coffee beans, well, in theory
+#' @param the bean comes from, default to Costa Rica
+#' @keywords bean
+#' @export
+#' @examples
+#' grind()
+
+grind <- function (bean = "Costa Rica") {
+  print("You've got bean from " + bean);
+}
